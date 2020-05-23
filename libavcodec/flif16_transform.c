f25caf9646c1b12432b0ce8583cecc7799d8c70a
daujerrine <aghorui@teknik.io>
2020-05-23 10:54:10 +0530

    Partial Cleanup and Suggestions on Transform Code please go through the comments

--- libavcodec/flif16_transform.c
+++ libavcodec/flif16_transform.c
@@ -3,156 +3,172 @@
 
 Transform* process(char* desc, FLIF16DecoderContext *s){
     Transform* transform;
-	if(desc == "YCoCg"){
-		transform = (Transform*)malloc(sizeof(Transform));
-		transform->s = s;
-		transform->desc = desc;
-		transform->transform_number = 1;
-		transform_read(transform);
-	}
-	else if(desc == "Permute"){
-		transform = (Transform*)malloc(sizeof(Transform));
-		transform->s = s;
-		transform->desc = desc;
-		transform->transform_number = 3;
-		transform_read(transform);
-	}
+    if (desc == "YCoCg") {
+        transform = (Transform*)malloc(sizeof(Transform));
+        transform->s = s;
+        transform->desc = desc;
+        transform->transform_number = 1;
+        transform_read(transform);
+    }
+    else if (desc == "Permute") {
+        transform = malloc(sizeof(Transform));
+        transform->s = s;
+        transform->desc = desc;
+        transform->transform_number = 3;
+        transform_read(transform);
+    }
     return transform;
 }
 
 uint8_t transform_read(Transform *transform){
     if(transform->desc == "YCoCg"){
-		//YCoCg data comprise of int origmax4, ColorRanges ranges 
-		transform->data_size = sizeof(int) + sizeof(ColorRanges);
-		transform->data = malloc(transform->data_size);
-		ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
-		ranges->num_planes = transform->s->channels;
-		return 1;
-	}
-	else if(transform->desc == "Permute"){
-		//PermutePlanes data comprise of flag uint8_t subtract, uint8_t permutation[5]
-		transform->data_size = 6*sizeof(uint8_t) + sizeof(ColorRanges);
-		transform->data = malloc(transform->data_size);
-		uint8_t *subtract = (uint8_t *)transform->data;
-		uint8_t *permutation = (uint8_t *)(transform->data + sizeof(uint8_t));
-		ColorRanges *ranges = (ColorRanges *)(transform->data  + 6*sizeof(uint8_t));
-		ranges->num_planes = transform->s->channels;
-		*subtract = ff_flif16_rac_read_nz_int(transform->s->rc, 0, 1);
-		uint8_t from[4] = {0, 0, 0, 0}, to[4] = {0, 0, 0, 0};
-		int p;
-		int planes = ranges->num_planes;
-		for(p = 0; p < planes; p++){
-			permutation[p] = ff_flif16_rac_read_nz_int(rac, 0, planes-1);
-			from[p] = 1;
-			to[p] = 1;
-		}
-		for(p = 0; p < planes; p++){
-			if(!from[p] || !to[p]){
-				return 0;
-			}
-		}
-		return 1;
-	}
-	return 0;
+        //YCoCg data comprise of int origmax4, ColorRanges ranges 
+        transform->data_size = sizeof(int) + sizeof(ColorRanges);
+        transform->data = malloc(transform->data_size);
+        ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
+        ranges->num_planes = transform->s->channels;
+        return 1;
+    }
+    else if(transform->desc == "Permute"){
+        //PermutePlanes data comprise of flag uint8_t subtract, uint8_t permutation[5]
+        transform->data_size = 6*sizeof(uint8_t) + sizeof(ColorRanges);
+        transform->data = malloc(transform->data_size);
+        uint8_t *subtract = (uint8_t *)transform->data;
+        uint8_t *permutation = (uint8_t *)(transform->data + sizeof(uint8_t));
+        ColorRanges *ranges = (ColorRanges *)(transform->data  + 6*sizeof(uint8_t));
+        ranges->num_planes = transform->s->channels;
+        *subtract = ff_flif16_rac_read_nz_int(transform->s->rc, 0, 1);
+        uint8_t from[4] = {0, 0, 0, 0}, to[4] = {0, 0, 0, 0};
+        int p;
+        int planes = ranges->num_planes;
+        for(p = 0; p < planes; p++){
+            permutation[p] = ff_flif16_rac_read_nz_int(rac, 0, planes-1);
+            from[p] = 1;
+            to[p] = 1;
+        }
+        for(p = 0; p < planes; p++){
+            if(!from[p] || !to[p]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+    return 0;
 }
 
 uint8_t transform_init(Transform *transform, ColorRanges *srcRanges){
-	if(transform->desc == "YCoCg"){
-		if(srcRanges->num_planes < 3)  return 0;
-		if(srcRanges->min[0] == srcRanges->max[0] || srcRanges->min[1] == srcRanges->max[1] || srcRanges->min[2] == srcRanges->max[2])    return 0;
-		if(srcRanges->min[0] < 1 || srcRanges->min[1] < 1 || srcRanges->min[2] < 1)  return 0;
-		int *origmax4 = (int *)transform->data;
-		*origmax4 = max(srcRanges->max[0], srcRanges->max[1], srcRanges->max[2])/4 -1;
-		ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
-		int p;
-		for(p = 0; p < ranges->num_planes; p++){
-			ranges->max[p] = srcRanges->max[p];
-			ranges->min[p] = srcRanges->min[p];
-		}
-		return 1;
-	}
-	else if(transform->desc == "Permute"){
-		if(srcRanges->num_planes < 3)  return 0;
-		if(srcRanges->min[0] < 1 || srcRanges->min[1] < 1 || srcRanges->min[2] < 1)  return 0;
-		ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
-		int p;
-		for(p = 0; p < ranges->num_planes; p++){
-			ranges->max[p] = srcRanges->max[p];
-			ranges->min[p] = srcRanges->min[p];
-		}
-		return 1;
-	}
+    if(transform->desc == "YCoCg"){
+        if(srcRanges->num_planes < 3) 
+            return 0;
+        if(   srcRanges->min[0] == srcRanges->max[0] 
+           || srcRanges->min[1] == srcRanges->max[1] 
+           || srcRanges->min[2] == srcRanges->max[2])
+            return 0;
+        if(   srcRanges->min[0] < 1 
+           || srcRanges->min[1] < 1 
+           || srcRanges->min[2] < 1) 
+            return 0;
+        
+        int *origmax4 = (int *) transform->data;
+        *origmax4 = max(srcRanges->max[0], 
+                        srcRanges->max[1], 
+                        srcRanges->max[2])/4 -1;
+
+        ColorRanges *ranges = (ColorRanges *) (transform->data + sizeof(int));
+        int p;
+        for (p = 0; p < ranges->num_planes; p++) {
+            ranges->max[p] = srcRanges->max[p];
+            ranges->min[p] = srcRanges->min[p];
+        }
+        return 1;
+    }
+
+    else if (transform->desc == "Permute") {
+        if (srcRanges->num_planes < 3)
+            return 0;
+        if(   srcRanges->min[0] < 1 
+           || srcRanges->min[1] < 1 
+           || srcRanges->min[2] < 1) 
+            return 0;
+        ColorRanges *ranges = (ColorRanges *) (transform->data + sizeof(int));
+        int p;
+        for(p = 0; p < ranges->num_planes; p++){
+            ranges->max[p] = srcRanges->max[p];
+            ranges->min[p] = srcRanges->min[p];
+        }
+        return 1;
+    }
 }
 
 uint8_t transform_forward(Transform *transform, interimPixelData* pixelData){
     if(transform->desc == "YCoCg"){
-		int r, c;
-		ColorVal R,G,B,Y,Co,Cg;
+        int r, c;
+        ColorVal R,G,B,Y,Co,Cg;
 
-		int height = pixelData->height;
-		int width = pixelData->width;
-		
-		for (r=0; r<height; r++) {
-			for (c=0; c<width; c++) {
-				R = pixelData->data[0][r*width + c];
-				G = pixelData->data[1][r*width + c];
-				B = pixelData->data[2][r*width + c];
+        int height = pixelData->height;
+        int width = pixelData->width;
+        
+        for (r=0; r<height; r++) {
+            for (c=0; c<width; c++) {
+                R = pixelData->data[0][r*width + c];
+                G = pixelData->data[1][r*width + c];
+                B = pixelData->data[2][r*width + c];
 
-				Y = (((R + B)>>1) + G)>>1;
-				Co = R - B;
-				Cg = G - ((R + B)>>1);
+                Y = (((R + B)>>1) + G)>>1;
+                Co = R - B;
+                Cg = G - ((R + B)>>1);
 
-				pixelData->data[0][r*width + c] = Y;
-				pixelData->data[1][r*width + c] = Co;
-				pixelData->data[2][r*width + c] = Cg;
-			}
-		}
-		
-		int *origmax4 = (int *)transform->data;
-		ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
-		//Will see if ranges need to be stored separately in transform struct only or not.
-		int p;
-		for(p = 0; p < ranges->num_planes; p++){
-			ranges->max[p] = max_range_YCoCg(p, *origmax4);
-			ranges->min[p] = min_range_YCoCg(p, *origmax4);
-		}
+                pixelData->data[0][r*width + c] = Y;
+                pixelData->data[1][r*width + c] = Co;
+                pixelData->data[2][r*width + c] = Cg;
+            }
+        }
+        
+        int *origmax4 = (int *)transform->data;
+        ColorRanges *ranges = (ColorRanges *)(transform->data + sizeof(int));
+        // Will see if ranges need to be stored separately in transform struct only or not.
+        int p;
+        for (p = 0; p < ranges->num_planes; p++) {
+            ranges->max[p] = max_range_YCoCg(p, *origmax4);
+            ranges->min[p] = min_range_YCoCg(p, *origmax4);
+        }
 
-		//I don't know yet what to do with this flag in forward and reverse transforms.
-		//transform->done = 1;
-		return 1;
-	}
-	else if(transform->desc == "Permute"){
-		//PENDING WORK
-		//Actual forward transform is yet to be written
-		uint8_t *subtract = (uint8_t *)transform->data;
-		uint8_t *permutation = (uint8_t *)(transform->data + sizeof(uint8_t));
-		ColorRanges *ranges = (ColorRanges *)(transform->data  + 6*sizeof(uint8_t));
-		ranges->num_planes = transform->s->channels;
+        //I don't know yet what to do with this flag in forward and reverse transforms.
+        //transform->done = 1;
+        return 1;
+    }
+    else if (transform->desc == "Permute") {
+        //PENDING WORK
+        //Actual forward transform is yet to be written
+        uint8_t *subtract = (uint8_t *)transform->data;
+        uint8_t *permutation = (uint8_t *)(transform->data + sizeof(uint8_t));
+        ColorRanges *ranges = (ColorRanges *)(transform->data  + 6*sizeof(uint8_t));
+        ranges->num_planes = transform->s->channels;
 
-		if((*subtract)){
-			if(ranges->num_planes > 3){
-				ranges->min[3] = ranges->min[permutation[3]];
-				ranges->max[3] = ranges->max[permutation[3]];
-			}
+        if (*subtract) {
+            if (ranges->num_planes > 3) {
+                ranges->min[3] = ranges->min[permutation[3]];
+                ranges->max[3] = ranges->max[permutation[3]];
+            }
 
-			ranges->max[1] = ranges->max[1] - ranges->min[0];
-			ranges->min[1] = ranges->min[1] - ranges->max[0];
-			ranges->max[2] = ranges->max[2] - ranges->min[0];
-			ranges->min[2] = ranges->min[2] - ranges->max[0];
-		}
-		return 1;
-	}
-	return 0;
+            ranges->max[1] = ranges->max[1] - ranges->min[0];
+            ranges->min[1] = ranges->min[1] - ranges->max[0];
+            ranges->max[2] = ranges->max[2] - ranges->min[0];
+            ranges->min[2] = ranges->min[2] - ranges->max[0];
+        }
+        return 1;
+    }
+    return 0;
 }
 
-uint8_t transform_reverse(Transform* transform, interimPixelData *pixelData){
+uint8_t transform_reverse(Transform* transform, interimPixelData *pixelData) {
     int r, c;
     ColorVal R,G,B,Y,Co,Cg;
 
-	int height = pixelData->height;
+    int height = pixelData->height;
     int width = pixelData->width;
     
-	for (r=0; r<height; r++) {
+    for (r=0; r<height; r++) {
         for (c=0; c<width; c++) {
             Y = pixelData->data[0][r*width + c];
             Co= pixelData->data[1][r*width + c];
@@ -167,6 +183,6 @@ uint8_t transform_reverse(Transform* transform, interimPixelData *pixelData){
             pixelData->data[2][r*width + c] = B;
         }
     }
-	return 0;
+    return 0;
 }
 
--- libavcodec/flif16_transform.h
+++ libavcodec/flif16_transform.h
@@ -1,37 +1,120 @@
+/*
+ * Transforms for FLIF16.
+ * Copyright (c) 2020 Kartik K. Khullar <kartikkhullar@gmail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file
+ * Transforms for FLIF16.
+ */
+ 
 #include <stdint.h>
+
 #include "../libavutil/frame.h"
-//#include "flif16dec.h"
+#include "flif16.h"
+
 #define MAX_PLANES 5
 
-typedef int16_t ColorVal;
+typedef int16_t FLIF16ColorVal;
 
-typedef struct{
-   	ColorVal min[MAX_PLANES], max[MAX_PLANES];
+typedef struct {
+   	FLIF16ColorVal min[MAX_PLANES], max[MAX_PLANES];
     int num_planes;
-}ColorRanges;
+} FLIF16ColorRanges;
 
-char* TransDesc[13] = {"ChannelCompact", "YCoCg", "?", "Permute", "Bounds", "PaletteAlpha", "Palette", "ColorBuckets", "?", "?", "DuplicateFrame", "FrameShape", "FrameLookback"};
+typedef enum FLIF16TransformTypes {
+    FLIF16_TRANSFORM_CHANNELCOMPACT = 0,
+    FLIF16_TRANSFORM_YCOCG,
+    FLIF16_TRANSFORM_RESERVED1,
+    FLIF16_TRANSFORM_PERMUTEPLANES,
+    FLIF16_TRANSFORM_BOUNDS,
+    FLIF16_TRANSFORM_PALETTEALPHA,
+    FLIF16_TRANSFORM_PALETTE,
+    FLIF16_TRANSFORM_COLORBUCKETS,
+    FLIF16_TRANSFORM_RESERVED2,
+    FLIF16_TRANSFORM_RESERVED3,
+    FLIF16_TRANSFORM_DUPLICATEFRAME,
+    FLIF16_TRANSFORM_FRAMESHAPE,
+    FLIF16_TRANSFORM_FRAMELOOKBACK,
+}
 
-typedef struct Transform{
-    char* desc;                     //Description of transform
-    uint8_t transform_number;       
+char* flif16_transform_desc[] = {
+    "ChannelCompact",
+    "YCoCg", 
+    "?",
+    "Permute",
+    "Bounds",
+    "PaletteAlpha",
+    "Palette", 
+    "ColorBuckets", 
+    "?", 
+    "?", 
+    "DuplicateFrame", 
+    "FrameShape", 
+    "FrameLookback"
+};
+
+typedef struct FLIF16Transform {
+    char* desc;                     //Description of FLIF16Transform
+    uint8_t FLIF16Transform_number;       
     uint8_t done;
     FLIF16DecoderContext *s;
-
     int data_size;
     void *data;
-}Transform;
+} FLIF16Transform;
 
 typedef struct{
     uint8_t initialized;            //FLAG : initialized or not.
     int height, width;
-    ColorVal *data[MAX_PLANES];
-    ColorRanges ranges;
-}interimPixelData;
+    FLIF16ColorVal *data[MAX_PLANES];
+    FLIF16ColorRanges ranges;
+} FLIF16InterimPixelData;
 
-ColorRanges* getRanges(interimPixelData* pixelData, ColorRanges *ranges);
+FLIF16ColorRanges* getRanges(FLIF16InterimPixelData* pixelData, 
+                             FLIF16ColorRanges *ranges);
 
-int getmax(ColorRanges* ranges, int p);
+int getmax(FLIF16ColorRanges* ranges, int p);
+
+// All of these are local functions, which are never used outsider the internal
+// functions of the transforms. Please prefix the function names with ff_*
+// and the return values with static. For example, max_range_YCoCg becomes:
+//      static int ff_max_range_ycocg(int p, int origmax 4.
+// You can also make these functions inline, if the function is small enough
+// which will increase speed.
+// All private functions have the ff_* prefix. Also the names must be kept in
+// lowercase as per convention.
+
+// Also, indentation is 4 spaces. Your editor is inserting tabs instead. Please
+// configure your editor to do so. Tabs are not of uniform width across editors.
+
+// Please try to keep everything within 80 columns by breaking long statements.
+// Your editor may hae an option to draw a vertical line at 80 columns.
+
+// This commit will most likely prevent this code from compiling. If you want to
+// revert to the previous version, please use git reset.
+
+// You might have missed my comment on gitbub over here:
+// https://github.com/daujerrine/ffmpeg/commit/341ce0ef2e00ee3f5ae9cc8dfd3b9d21d1686476#commitcomment-39077413
+// You might have to click on "watch" in order to get email notifications for
+// comments.
+
+// Please remove these comments afterwards.
 
 int max_range_YCoCg(int p, int origmax4);
 int min_range_YCoCg(int p, int origmax4);
@@ -45,15 +128,64 @@ int get_max_co(int orgimax4, int yval);
 int get_min_cg(int orgimax4, int yval, int coval);
 int get_max_cg(int orgimax4, int yval, int coval);
 
-int min(int, int);
-int max(int, int, int);
+int min(int, int);  //* Replace by FF_MIN
+int max(int, int, int); //* Replace by FF_MAX3 https://ffmpeg.org/doxygen/trunk/common_8h.html
 
-//ColorRanges crangesYCoCg(int p, ColorVal* prevPlanes, TransformYCoCg transform);
+//FLIF16ColorRanges crangesYCoCg(int p, FLIF16ColorVal* prevPlanes, FLIF16TransformYCoCg FLIF16Transform);
 
-Transform* process(int t_no, FLIF16DecoderContext *s);
+FLIF16Transform* ff_flif16_transform_process(int t_no, FLIF16DecoderContext *s);
 
-uint8_t transform_read(Transform *transform);
-uint8_t transform_init(Transform *transform, ColorRanges *ranges);
-uint8_t transform_forward(Transform *transform, interimPixelData *pixelData);
-uint8_t transform_reverse(Transform *transform, interimPixelData *pixelData);
+uint8_t ff_flif16_transform_read(FLIF16Transform *transform);
+uint8_t ff_flif16_transform_init(FLIF16Transform *transform,
+                                 FLIF16ColorRanges *ranges);
+uint8_t ff_flif16_transform_forward(FLIF16Transform *transform, 
+                                    FLIF16InterimPixelData *pixelData);
+uint8_t ff_flif16_transform_reverse(FLIF16Transform *transform, 
+                                    FLIF16InterimPixelData *pixelData);
 
+
+/*
+Maybe write it like this:
+
+typedef struct FLIF16TransformContext {
+    uint8_t done;
+    FLIF16DecoderContext *s;
+    void *priv_data;
+}
+
+typedef struct FLIF16Transform {
+    char desc[15];
+    // These are functions
+    uint8_t (*init) (FLIF16TransformContext *);
+    uint8_t (*read) (FLIF16TransformContext *);
+    uint8_t (*forward) (FLIF16TransformContext *);
+    uint8_t (*reverse) (FLIF16TransformContext *);
+    size_t priv_data_size;
+} FLIF16Transform;
+
+FLIF16Transform *flif16_transforms[] {
+    ...
+    &flif16_transform_ycocg,
+    &flif16_transform_permuteplanes,
+    ...
+}
+
+int ff_flif16_transform_process(unsigned int t_no, int direction, 
+                                FLIF16DecoderContext *s)
+{
+    FLIF16Transform *t = flif16_transforms[t_no];
+    FLIF16TransformContext ctx = {
+        .priv_data = av_mallocz(t->priv_data_size);
+        .s = s; // Maybe use a more elaborate name
+    }
+
+    // Perform error handling as required over here by returning an integer
+    t->init(&ctx);
+    t->read(&ctx);
+    t->forward(&ctx);
+    t->reverse(&ctx);
+    
+    return 1;
+}
+ 
+*/
